7d23599f72f2d49e261ef6eeae6af46e686c7db9
DaneVN <Dane.vN23@gmail.com>
2024-09-12 16:01:00 +0200

    Comments UPDATED

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Array of song objects. Add at least 5 songs with title, artist, and genre properties.
+//TASK: Array of song objects. Add at least 5 songs with title, artist, and genre properties.
 const songs = [
     { title: "Hooked on a Feeling", artist: "Blue Swede", genre: "Pop" },
     { title: "Moonage Daydream", artist: "David Bowie", genre: "Rock" },
@@ -11,7 +11,7 @@ const songs = [
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
-    // Feel free to add even more songs
+    //TASK: Feel free to add even more songs
     { title: "Mr. Blue Sky", artist: "Electric Light Orchestra", genre: "R&B" },
     { title: "The Chain", artist: "Fleetwood Mac", genre: "Rock" },
     { title: "Come and Get Your Love (Rerecorded)", artist: "Redbone", genre: "R&B" },
@@ -20,21 +20,20 @@ const songs = [
 ];
 
 
-// Object containing each Guardian's preferred genre
+//TASK: Object containing each Guardian's preferred genre
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
-    // Add preferences for Drax, Rocket, and Groot
+    //TASK: Add preferences for Drax, Rocket, and Groot
     "Drax" : "R&B",
     "Rocket" : "Rock",
     "Groot" : "R&B",
-    //Student Note: What about Mantis? :(
     "Mantis" : "Pop"
 };
 
-// Function to generate playlist based on preferred genre
+//TASK: Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
-    // Use the map() function to create playlists for each Guardian
+    //TASK: Use the map() function to create playlists for each Guardian
     return Object.entries(guardians).map(([person, genre]) => {
         //Search through the songs array for songs that mach genre preference
         const playlist = songs.filter((song) => (genre) === song.genre);
@@ -43,17 +42,15 @@ function generatePlaylist(guardians, songs) {
     );
 }
 
-// Call generatePlaylist and display the playlists for each Guardian
+//TASK: Call generatePlaylist and display the playlists for each Guardian
 const playlists = generatePlaylist(guardians, songs);
 
 document.querySelector('#playlists').innerHTML = playlists.map(
-  (entry) => //Make it pretty
-  `
-    <p><strong>${entry.person}'s Playlist:</strong></p>
-    <ol style="list-style-type: none">
+  (entry) => 
+    //Add some layout/styling and use map() to add a listitem for each songin the output
+  ` <p><strong>${entry.person}'s Playlist:</strong></p>
+    <ul style="list-style-type: none">
       ${entry.playlist.map(song => `<li>${song.title} (${song.genre})</li>`).join('')}
-    </ol>
+    </ul>
   `
 ).join('');
-
-
